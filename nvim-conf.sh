80f451f172f171fb11a646b882e616f4a7e3de0a
corei8 <corei8.github@gmail.com>
2022-05-01 17:24:59 -0400

    Add .alias file

---
+++ nvim-conf.sh
@@ -0,0 +1,7 @@
+#! /bin/zsh
+# Adjust init.vim from anywhere
+# This is not working right now!!!
+echo "Working on opening nvim..."
+lvar=`cd ../.config/nvim/`
+var=`nvim ../.config/nvim/init.vim`
+exit $var
